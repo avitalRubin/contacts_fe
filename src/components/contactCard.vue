8c66e982ce5ecfacd40957713aa576398136c20c
avitalRubin <59803121+avitalRubin@users.noreply.github.com>
2022-08-04 21:05:35 +0300

    Add files via upload

---
+++ src/components/contactCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-card
+      fluid
+      class="pa-0"
+  >
+    <v-row align="center">
+      <v-col
+          cols="12"
+          sm="4"
+      >
+        <div class="text-center">
+          <div class="ma-4">
+            <v-avatar
+                size="140"
+            >
+              <img
+                  src="../assets/male-avatar.png"
+                  alt="user"
+              >
+            </v-avatar>
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="ma-4">
+            avital
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="ma-4">
+            avit123
+          </div>
+        </div>
+      </v-col>
+      <v-col
+          cols="12"
+          sm="4"
+      >
+        <div class="text-center">
+          <div class="my-2">
+            full name
+          </div>
+          <div class="my-2">
+            email
+          </div>
+          <div class="my-2">
+            phone number
+          </div>
+          <div class="my-2">
+            address
+          </div>
+        </div>
+      </v-col>
+      <v-col
+          cols="12"
+          sm="2"
+      >
+        <div class="text-center">
+          <div class="ma-4">
+            <v-btn
+                block
+                large
+                color="primary"
+                dark
+            >
+              info
+            </v-btn>
+          </div>
+
+          <div class="ma-4">
+            <v-btn
+                block
+                color="error"
+                dark
+                large
+            >
+              delete
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/viewAllContacts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <contactCard/> <!--todo - loop over contacts and send contact data to component-->
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import contactCard from '../components/contactCard';
+export default {
+  data() {
+    return {
+      contacts: {}
+    };
+  },
+  components: {
+    contactCard,
+  },
+  async created() {
+    try {
+      const response = await axios.get(
+          "http://localhost:3000/get_contact_list"
+      );
+      // JSON responses are automatically parsed.
+      this.contacts = response.data.contacts;
+      console.log(this.contacts[0])
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
